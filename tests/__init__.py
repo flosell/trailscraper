9bc8c6aa22c149f41fb97ad9c33474854329fad1
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-12-03 11:58:03 +0100

    Monkey-patch repr onto awacs objects for better messages in test failures

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,10 @@
 import sys
 import os
+
+from awacs import AWSObject, AWSHelperFn
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+AWSObject.__repr__ = lambda self: str(self.JSONrepr())
+AWSHelperFn.__repr__ = lambda self: str(self.to_json())
