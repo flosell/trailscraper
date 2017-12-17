59a815ba8a2704eed5c5a605143d64bcd4c4e8e2
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-12-17 13:10:42 +0100

    Check support for Python 3.3 #2

--- setup.py
+++ setup.py
@@ -59,9 +59,10 @@ setup(
         'Topic :: Security',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.3',
 )
