6d8a57fe5c6d421dace7a6ddf49fc4c4b4f72e25
Florian Sellmayr <florian.sellmayr@gmail.com>
2026-04-06 15:34:18 +0200

    Fix test - exit code is 2 for all help messages in more recent click versions

--- tests/integration/cli_help_test.py
+++ tests/integration/cli_help_test.py
@@ -6,5 +6,5 @@ from trailscraper import cli
 def test_should_output_help_message_by_default():
     runner = CliRunner()
     result = runner.invoke(cli.root_group)
-    assert result.exit_code == 0
+    assert result.exit_code == 2
     assert 'Usage:' in result.output
